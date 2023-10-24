744babfb51f4de567a1b150588b6f892bb258c5a
Yusuf Semo <141759104+yusufSemo@users.noreply.github.com>
2023-10-24 18:45:29 -0500

    Add files via upload

---
+++ P08/full_credit/hello_all.cpp
@@ -0,0 +1,10 @@
+//yusuf semo P08
+#include <iostream>
+
+int main(){
+    std::string name;
+    std::cout << "What's your name? ";
+    std::cin >> name;
+    std::cout << "Hello, "<< name <<"!"<<std::endl;
+    return 0;
+}
