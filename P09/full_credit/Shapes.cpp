023180a8f885db9faed169b64a38d611acc97d53
Yusuf Semo <yas3176@mavs.uta.edu>
2023-11-01 15:26:05 -0500

    3

--- P09/full_credit/Shapes.cpp
+++ P09/full_credit/Shapes.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
 #include <string>
-#include "Shape.h"
+
+#include "hexagon.h"
 
 int main(void){
     Shape shape;
     std::cout<<shape.to_string()<<std::endl;
+    hexagon pol(6.0);
+    std::cout<<pol.to_string()<<std::endl;
+
 }
