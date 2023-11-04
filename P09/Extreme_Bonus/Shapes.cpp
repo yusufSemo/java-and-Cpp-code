46c88988059abf45cafe08b754087775ee5e8496
Yusuf Semo <yas3176@mavs.uta.edu>
2023-11-04 16:25:59 -0500

    extreme bonus

--- P09/Extreme_Bonus/Shapes.cpp
+++ P09/Extreme_Bonus/Shapes.cpp
@@ -4,57 +4,41 @@
 #include "Circle.h"
 #include "hexagon.h"
 #include "Shape.h"
-class LineDrawingArea : public Gtk::DrawingArea {
+
+class CircleDrawingArea : public Gtk::DrawingArea {
+private:
+    double _radius;
+
 public:
-    LineDrawingArea() {
+    CircleDrawingArea(double radius) : _radius{radius} {
         // Connect the drawing callback
-        signal_draw().connect(sigc::mem_fun(*this, &LineDrawingArea::on_draw), false);
+        signal_draw().connect(sigc::mem_fun(*this, &CircleDrawingArea::on_draw), false);
     }
 
 protected:
     bool on_draw(const Cairo::RefPtr<Cairo::Context>& cr) override {
-        cr->set_source_rgb(0.0, 0.0, 0.0);//background color
-        // Create a rectangle that covers the entire drawing area
-        double width = get_allocated_width();
-        double height = get_allocated_height();
-        cr->rectangle(0, 0, width, height);
-        // Fill the rectangle with the background color
-         cr->fill();
-        // Set line color and properties
-        cr->set_source_rgb(1.0, 0.5, 0.0);  // Black color
+        // Set the color for the circle's outline
+        cr->set_source_rgb(1.0, 0.5, 0.0);  // Orange color (R=1.0, G=0.5, B=0.0)
+
+        // Set the line width for the outline
         cr->set_line_width(2.0);
 
-        // Draw the line from (x1, y1) to (x2, y2)
-        double x1 = 50.0;
-        double y1 = 50.0;
-        double x2 = 200.0;
-        double y2 = 100.0;
-
-        cr->move_to(x1, y1);
-        cr->line_to(x2, y2);
+        // Calculate the circle's center and radius
+        double center_x = get_allocated_width() / 2.0;
+        double center_y = get_allocated_height() / 2.0;
+        double radius = std::min(center_x, center_y) * 0.2;
+        cr->arc(center_x, center_y, _radius, 0.0, 2.0 * M_PI);
         cr->stroke();
 
         return true;
     }
 };
-int main(int argc,char* argv[]){
-    auto app =
-    Gtk::Application::create(argc, argv,
-      "org.gtkmm.examples.base");
 
-  Gtk::Window window;
-  window.set_default_size(420, 420);
-LineDrawingArea drawing_area;
-    window.add(drawing_area);
-
-    window.show_all();
-  return app->run(window);
-
-
-   
+int main(int argc, char* argv[]) {
     std::vector<Shape*> shapes;
+    double cir1Radious = 100;
     hexagon pol(6.0);
-    Circle cir(6.0);
+    Circle cir(cir1Radious);
     Circle cir2(99.0);
     shapes.push_back(&pol);
     shapes.push_back(&cir);
@@ -62,5 +46,19 @@ LineDrawingArea drawing_area;
     for (Shape* shape : shapes) {
         std::cout << shape->to_string() << std::endl;
     }
-    return 0;
+    auto app = Gtk::Application::create(argc, argv, "org.gtkmm.examples.base");
+
+    Gtk::Window window;
+    window.set_default_size(420, 420);
+
+    Gtk::Box vbox(Gtk::ORIENTATION_VERTICAL); // Vertical box
+
+    CircleDrawingArea drawing_area(cir1Radious);
+
+    vbox.pack_start(drawing_area);
+    window.add(vbox);
+
+    window.show_all();
+
+    return app->run(window);
 }
